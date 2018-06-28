8459c0a35a20fa0c2040dc8ed48ec5bd9adb5e08
Ealon1996 <38780025+Ealon1996@users.noreply.github.com>
2018-06-28 19:25:05 +0800

    try create data&label

--- test.py
+++ test.py
@@ -6,20 +6,35 @@ Created on Tue Jun 26 01:10:22 2018
 """
 
 from PIL import Image
-import numpy
+import numpy 
 import cPickle
 import cv2
 import os,glob
 import shutil
-fs='/data/teamp8/ML/face/s01_01.jpg'
-face_data = numpy.empty((750,220,110))
-for row in range(1,51):
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
 
-    for col in range(1,16):
-        if col==14: 
-          
-          fs=fs[:22]+'{:0>2d}'.format(row)+fs[24]+'{:0>2d}'.format(col)+fs[27:]
-          newname='s'+'{:0>2d}'.format(row)+'_09.jpg' 
-          print newname
-          shutil.copy2(fs,newname)
+fs='/data/teamp8/ML/face/s01_01.jpg'
+face_data = numpy.empty((50*15,220,110))
+for row in range(50):
+
+    for col in range(15):
+        p=row+1
+        n=col+1
+        fs=fs[:22]+'{:0>2d}'.format(p)+fs[24]+'{:0>2d}'.format(n)+fs[27:]
+        img = Image.open(fs)
+        img = img.resize( (110, 220), Image.BILINEAR )
+        img = img.convert('L')
+        img_ndarray = numpy.asarray(img, dtype='float64')/ 255
+        face_data[row*15+col] =img_ndarray
         
+x = face_data
+print (x.shape)
+face_label = numpy.empty(750)
+for i in range(750):
+  face_label[i]= int(i/15)
+y = face_label
+print (y.shape)
